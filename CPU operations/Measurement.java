1b6aae326950711a3a2d3ac8604c2b29c411a9c8
violetchen9 <violetchen9@gmail.com>
2016-04-17 16:24:01 -0700

    Change Measurement Overhead

--- CPU operations/Measurement.java
+++ CPU operations/Measurement.java
@@ -2,26 +2,30 @@ package OSproject;
 
 public class Measurement {
 	
+	private final static int MEASURE_NUM = 1000;
 	public static void main(String[] args) {
 		
 		double start, end;
-		    
-		start = System.nanoTime();	
-		end = System.nanoTime();
+		double diff = 0.0;  
+		for (int i = 0 ; i < MEASURE_NUM ; i++){
+			start = System.nanoTime();	
+			end = System.nanoTime();
+			diff += end - start;
+		}
 			
-		System.out.println("Reading time is " + (end - start));
+		System.out.println("Reading time is " + diff / MEASURE_NUM);
 		System.out.println("Loop number    Loop time");
 		int loop_num = 1;
-		for (int measure_num = 0; measure_num < 10 ; measure_num ++){
-			double diff = 0.0;
-			for (int k = 0 ; k < 100 ; k++){
+		for (int i = 0; i < 10 ; i ++){
+			diff = 0.0;
+			for (int j = 0 ; j < MEASURE_NUM ; j++){
 				start = System.nanoTime();		
-				for (int i = 0; i < loop_num; i++) {}
+				for (int k = 0; k < loop_num; k++) {}
 				end = System.nanoTime();
 			
 				diff += (end - start); 
 			}
-			System.out.println( loop_num + "                " + diff/100);
+			System.out.println( loop_num + "                " + diff / MEASURE_NUM);
 			loop_num *= 2;
 		}
 			return;
